07cabdb8fad8cad586bce1902806215b515696fa
dolphinmind <dolphinmind@foxmail.com>
2024-08-02 11:23:30 +0800

    feat:behavior-mediator-pattern-类DefaultSqlSession实现SqlSession接口

---
+++ behavior/src/main/java/com/github/dolphinmind/behavior/mediator/mediator/impl/DefaultSqlSession.java
@@ -0,0 +1,213 @@
+package com.github.dolphinmind.behavior.mediator.mediator.impl;
+
+import com.github.dolphinmind.behavior.mediator.mediator.SqlSession;
+import com.github.dolphinmind.behavior.mediator.mediator.XNode;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.sql.*;
+import java.sql.Date;
+import java.util.*;
+
+/**
+ * @author dolphinmind
+ * @ClassName DefaultSqlSession
+ * @description
+ * @date 2024/8/2
+ */
+
+public class DefaultSqlSession implements SqlSession {
+
+    private Connection connection;
+    private Map<String, XNode> mapperElement;
+
+    public DefaultSqlSession(Connection connection, Map<String, XNode> mapperElement) {
+       this.connection    = connection;
+       this.mapperElement = mapperElement;
+    }
+
+    @Override
+    public <T> T selectOne(String statement) {
+
+        try {
+            XNode xNode = mapperElement.get(statement);
+            PreparedStatement preparedStatement = connection.prepareStatement(xNode.getSql());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<T> objects = resultSet2Obj(resultSet, Class.forName(xNode.getResultType()));
+            if (objects.size() == 1) {
+                return objects.get(0);
+            } else {
+                throw new RuntimeException("查询结果为空或者返回结果过多");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public <T> T selectOne(String statement, Object parameter) {
+        XNode xNode = mapperElement.get(statement);
+        Map<Integer, String> parameterMap = xNode.getParameter();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(xNode.getSql());
+            buildParameter(preparedStatement, parameter, parameterMap);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<T> objects = resultSet2Obj(resultSet, Class.forName(xNode.getResultType()));
+            if (objects.size() == 1) {
+                return objects.get(0);
+            } else {
+                throw new RuntimeException("查询结果为空或者返回结果过多");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public <T> List<T> selectList(String statement) {
+        XNode xNode = mapperElement.get(statement);
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(xNode.getSql());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet2Obj(resultSet, Class.forName(xNode.getResultType()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public <T> List<T> selectList(String statement, Object parameter) {
+        XNode xNode = mapperElement.get(statement);
+        Map<Integer, String> parameterMap = xNode.getParameter();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(xNode.getSql());
+            buildParameter(preparedStatement, parameter, parameterMap);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet2Obj(resultSet, Class.forName(xNode.getResultType()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将结果集转换为对象
+     * @param resultSet
+     * @param clazz
+     * @return
+     * @param <T>
+     */
+    private <T> List<T> resultSet2Obj(ResultSet resultSet, Class<?> clazz) {
+        List<T> list = new ArrayList<>();
+
+        try {
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            // 每次遍历行值
+            while (resultSet.next()) {
+                T obj = (T) clazz.newInstance();
+                for (int i = 1; i <= columnCount; i++) {
+                    Object value = resultSet.getObject(i);
+                    String columnName = metaData.getColumnName(i);
+                    String setMethod = "set" + columnName.substring(0, 1).toUpperCase() + columnName.substring(1);
+                    Method method;
+                    if (value instanceof Timestamp) {
+                        method = clazz.getMethod(setMethod, Date.class);
+                    } else {
+                        method = clazz.getMethod(setMethod, value.getClass());
+                    }
+                    method.invoke(obj, value);
+                }
+                list.add(obj);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+
+    /**
+     * 构建参数
+     * @param preparedStatement
+     * @param parameter
+     * @param parameterMap
+     * @throws SQLException
+     * @throws IllegalAccessException
+     */
+    private void buildParameter(PreparedStatement preparedStatement, Object parameter, Map<Integer, String> parameterMap) throws SQLException, IllegalAccessException {
+        int size = parameterMap.size();
+
+        // 单个参数
+        if (parameter instanceof Long) {
+            for (int i = 1; i <= size; i++) {
+                preparedStatement.setLong(i, Long.parseLong(parameter.toString()));
+            }
+            return;
+        }
+
+        if (parameter instanceof Integer) {
+            for (int i = 1; i <= size; i++) {
+                preparedStatement.setInt(i, Integer.parseInt(parameter.toString()));
+            }
+            return;
+        }
+
+        if (parameter instanceof String) {
+            for (int i = 1; i <= size; i++) {
+                preparedStatement.setString(i, parameter.toString());
+            }
+            return;
+        }
+
+        Map<String, Object> fieldMap = new HashMap<>();
+        // 对象参数
+        Field[] declaredFields = parameter.getClass().getDeclaredFields();
+        for (Field field : declaredFields) {
+            String name = field.getName();
+            field.setAccessible(true);
+            Object obj = field.get(parameter);
+            field.setAccessible(false);
+            fieldMap.put(name, obj);
+        }
+
+        for (int i = 1; i <= size; i++) {
+            String parameterDefine = parameterMap.get(i);
+            Object obj = fieldMap.get(parameterDefine);
+
+            if (obj instanceof Short) {
+                preparedStatement.setShort(i, Short.parseShort(obj.toString()));
+                continue;
+            }
+
+            if (obj instanceof Integer) {
+                preparedStatement.setInt(i, Integer.parseInt(obj.toString()));
+                continue;
+            }
+
+            if (obj instanceof Long) {
+                preparedStatement.setLong(i, Long.parseLong(obj.toString()));
+                continue;
+            }
+
+            if (obj instanceof String) {
+                preparedStatement.setString(i, obj.toString());
+                continue;
+            }
+
+            if (obj instanceof Date) {
+                preparedStatement.setDate(i, (java.sql.Date) obj);
+            }
+        }
+    }
+    @Override
+    public void close() {
+
+    }
+}
