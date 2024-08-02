cf20111fea2c14bdc9e0f596aaad453ac6a07559
dolphinmind <dolphinmind@foxmail.com>
2024-08-02 12:07:10 +0800

    feat:behavior-mediator-pattern-定义Resources类

---
+++ behavior/src/main/java/com/github/dolphinmind/behavior/mediator/mediator/support/Resources.java
@@ -0,0 +1,39 @@
+package com.github.dolphinmind.behavior.mediator.mediator.support;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/**
+ * @author dolphinmind
+ * @ClassName Resources
+ * @description
+ * @date 2024/8/2
+ */
+
+public class Resources {
+
+    public static Reader getResourceAsReader(String resource) throws IOException {
+        return new InputStreamReader(getResourceAsStream(resource));
+    }
+
+    private static InputStream getResourceAsStream(String resource) throws IOException{
+        ClassLoader[] classLoaders = getClassLoaders();
+        for (ClassLoader classLoader : classLoaders) {
+            InputStream inputStream = classLoader.getResourceAsStream(resource);
+            if (null != inputStream) {
+                return inputStream;
+            }
+        }
+        throw new IOException("Could not find resource " + resource);
+    }
+
+    private static ClassLoader[] getClassLoaders() {
+        return new ClassLoader[]{
+                ClassLoader.getSystemClassLoader(),
+                Thread.currentThread().getContextClassLoader()
+        };
+    }
+
+}
