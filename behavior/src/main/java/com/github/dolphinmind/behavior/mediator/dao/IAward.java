54b5d0c088175fea7cec3f2056e89cde15b65978
dolphinmind <dolphinmind@foxmail.com>
2024-08-02 13:34:01 +0800

    feat:behavior-mediator-pattern-Dao-创建IAward

---
+++ behavior/src/main/java/com/github/dolphinmind/behavior/mediator/dao/IAward.java
@@ -0,0 +1,16 @@
+package com.github.dolphinmind.behavior.mediator.dao;
+
+import com.github.dolphinmind.behavior.mediator.po.Award;
+
+/**
+ * @author dolphinmind
+ * @InterfaceName IAward
+ * @description
+ * @date 2024/8/2
+ */
+
+public interface IAward {
+
+    Award queryAwardInfoByAwardId(Long awardId);
+
+}
